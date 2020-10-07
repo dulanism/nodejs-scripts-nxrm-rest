ec0472e4eff91f7c36d123747ac55f6a597352fe
Dulani Wallace <dulani@dulani.com>
2020-10-07 00:36:32 -0400

    New roles file

---
+++ roles.js
@@ -0,0 +1,20 @@
+[
+{
+  "id": "atlanta-dev-role",
+  "name": "atlanta-dev-role",
+  "description": "atlanta dev team role",
+  "privileges": [
+      "nx-repository-admin-maven2-maven-central-*",
+      "nx-analytics-all"
+  ]
+},
+{
+  "id": "boston-dev-role",
+  "name": "boston-dev-role",
+  "description": "Boston dev team role",
+  "privileges": [
+      "nx-repository-admin-maven2-maven-central-*",
+      "nx-analytics-all"
+  ]
+}
+]
