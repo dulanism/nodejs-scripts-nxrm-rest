b0b913d6940a74d7142999c7c71920181ad21873
Dulani Wallace <dulani@dulani.com>
2020-10-07 00:02:01 -0400

    Add shared data file

---
+++ shared-data.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    contentType: {
+        'contentType': 'Content-Type',
+        'Json': 'application/json'
+    },
+    endpoint: "http://localhost:8081/service/rest/v1/security/roles",
+    u: "admin",
+    p: "popsicle"
+}
