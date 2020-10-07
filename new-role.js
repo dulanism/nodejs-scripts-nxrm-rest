0cc55598563455ce41386e983b5182752bf5a6da
Dulani Wallace <dulani@dulani.com>
2020-10-07 00:05:36 -0400

    updated / uncommented script

--- new-role.js
+++ new-role.js
@@ -1,26 +1,12 @@
 const request = require('superagent')
 const d = require('./shared-data.js')
-const fs = require('fs')
 
-const stream = fs.createReadStream('./roles.json')
-const req = request.post('d.endpoint')
-
-.then(response => {
+request.post(d.endpoint)
+    .auth(d.u, d.p)
+    .set(d.contentType.contentType, d.contentType.Json)
+    .send('{"id":"Atlanta-dev-role","name":"Atlanta-dev-role","description": "Atlanta dev team role","privileges":["nx-repository-admin-maven2-maven-central-*","nx-analytics-all"]}')
+    .then(response => {
+        console.log(response.body);
     }).catch(error => {
         console.log(JSON.stringify(error, null, 2));
-});
-
-
-
-// const request = require('superagent')
-// const d = require('./shared-data.js')
-
-// request.post(d.endpoint)
-//     .auth(d.u, d.p)
-//     .set(d.contentType.contentType, d.contentType.Json)
-//     .send('{"id":"Atlanta-dev-role","name":"Atlanta-dev-role","description": "Atlanta dev team role","privileges":["nx-repository-admin-maven2-maven-central-*","nx-analytics-all"]}')
-//     .then(response => {
-//         console.log(response.body);
-//     }).catch(error => {
-//         console.log(JSON.stringify(error, null, 2));
-//     });
+    });
