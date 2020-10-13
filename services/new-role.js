00f9221380885a966c22c32c7bb7e05096d168eb
Dulani Wallace <dulani@dulani.com>
2020-10-13 15:34:49 -0400

    Updates file structure

---
+++ services/new-role.js
@@ -0,0 +1,30 @@
+const request = require('superagent')
+const { user, password, endpoint } = require('./shared-data.js')
+const roles = require('./models/roles.js');
+
+// console.log(roles) Check for post
+
+// const promises = roles.map(item =>
+//     request.post(endpoint)
+//         .auth(user, password)
+//         .send(item)
+// )
+// Promise.all(promises).then(results => {
+//     results.map(item => console.log(item.body))
+// }).catch(e => console.log(e))
+
+//IIFE (immediately invoked function expression!)
+(async () => {
+    const promises = roles.map(item =>
+        request.post(endpoint)
+            .auth(user, password)
+            .send(item)
+    )
+    try {
+        const results = await Promise.all(promises)
+        results.map(item => console.log(item.body))
+    } catch (e) {
+        console.log(e.response.body)
+    }
+
+})() 
