4edff52bc2a13d0dfb8ef77fb017cea23d9acc52
Dulani Wallace <dulani@dulani.com>
2020-10-14 19:17:31 -0400

    Adds a new file and updates existing ones

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 module.exports = {
   rolesEndpoint:
-    process.env.ROLES_ENDPOINT || 'http://localhost:8081/service/rest/v1/security/roles',
+    process.env.ROLES_ENDPOINT
   //usersEndpoint: process.env.USERS_ENDPOINT || 'http://localhost:8081/service/rest/v1/security/users',
   user: 'admin',
   password: 'popsicle',
